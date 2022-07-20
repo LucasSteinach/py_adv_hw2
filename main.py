afcee8f8a28fc950a6de3367c482298c0aadc444
LucasSteinach <steinach@mail.ru>
2022-07-20 19:33:12 +0300

    #2

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ import re
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=',')
     contacts_list = list(rows)
-# pprint(contacts_list)
-# print('\n\n\n')
+print(contacts_list)
+print('\n\n\n')
 # TODO 1: выполните пункты 1-3 ДЗ
-string_list = [" ".join(i) for i in contacts_list]
-# pprint(string_list)
-new_list =  []
-people_dict = dict()
-for item in string_list[1:]:
-    # extract str 'Lastname Name Surname'
-    fullname = re.findall(r'[а-яёА-ЯЁ\s]+[^a-zA-Z ]', re.split(r'ФНС|Минфин', item)[0])
-    if fullname[0] not in people_dict.keys():
-        people_dict[fullname[0]] = []
-print(people_dict)
-    # fills first 3 position
-    # new_list.append([i for i in fullname[0].split(' ')[:3] ])
-print(new_list)
+for contact in contacts_list:
+    fullname = ' '.join(contact[:3])
+    sep_name = re.findall(r'\w+', fullname)
+    for i in range(len(sep_name)):
+        contact[i] = sep_name[i]
 
+    pattern = re.compile(r"(\+7|8)[\s\(]*(\d{3})[\s\)-]*(\d{3})-?(\d{2})-?(\d{2})[\(доб[\.\s]*(\d*)\)?")
+    if contact[5] != '':
+        main_phone = pattern.sub(r'+7(\2)\3-\4-\5', contact[5])
+        add_phone = pattern.sub(r'\6', contact[5])
+        if len(add_phone) == 0:
+            contact[5] = main_phone
+        else:
+            contact[5] = main_phone + ' доб.' + add_phone
+
+
+pprint(contacts_list)
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 # with open("phonebook.csv", "w") as f:
